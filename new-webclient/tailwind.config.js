3f57e603b2393d65b39bc4a2d0b0a882c64c9226
Unmani Shinde <mailto.unmani@gmail.com>
2024-04-09 14:31:12 +0530

    Landing page added in new client

---
+++ new-webclient/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [
+    flowbite.plugin(),
+  ],
+}
